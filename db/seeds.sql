95ea529c412c59f7e87c718c4101f03750a1b402
wesley9mm <info.developer4@gmail.com>
2018-07-24 22:14:19 -0700

    add

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+SELECT * FROM burgers;
+
+-- Inserted a set of records into the table
+INSERT INTO burgers (burger_name, devoured) VALUES ("Dbl Cheese", "true");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon", "true");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken", "true");
+
