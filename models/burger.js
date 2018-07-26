aaeb539659a169d96b51bc497dc01680f6cbad5b
wesley9mm <info.developer4@gmail.com>
2018-07-26 10:06:45 -0700

    add

---
+++ models/burger.js
@@ -0,0 +1,17 @@
+var orm = require("../config/orm.js");
+
+var movie = {
+    findAll: function(cb) {
+        orm.findAll("burgers", function(data){
+            cb(data);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(data){
+            cb(data);
+        });
+    }
+};
+    
+
+module.exports = burgers;
