738ad16e3a8369fcdc99afd15f76df29c24b9d14
wesley9mm <info.developer4@gmail.com>
2018-07-26 23:25:07 -0700

    add

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,18 +2,18 @@ $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
+    var burger = {
+        addBurger: $("name").val().trim(),
+        Devour: $(true).val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/main.handlebars", {
         method: "POST",
         data: burgers
     }).then(
         function () {
-            console.log("created new cat");
+            console.log("created new burger");
             // Reload the page to get the updated list
             location.reload();
         }
